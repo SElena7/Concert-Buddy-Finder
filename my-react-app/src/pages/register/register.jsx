84f7cdd23c0e43a993eda46504a1f86bb48c481c
SElena7 <89201050@student.upr.si>
2023-10-22 20:19:27 +0200

    Register page design created

--- my-react-app/src/pages/register/register.jsx
+++ my-react-app/src/pages/register/register.jsx
@@ -6,7 +6,32 @@ const Register = () => {
 	return (
 
 		<div className="register">
-			register
+			<div className="card">
+				<div className="left">
+					<h1>Concert Buddy Finder</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					</p>
+					<span>
+						Do you already have an account?
+					</span>
+					<button>
+						Log in
+					</button>
+
+				</div>
+				<div className="right">
+					<h1>Register</h1>
+					<form>
+						<input type="text" placeholder="Username"></input>
+						<input type="email" placeholder="Email"></input>
+						<input type="password" placeholder="Password"></input>
+						<input type="text" placeholder="Name"></input>
+						<button>Register</button>
+
+					</form>
+				</div>
+			</div>
 		</div>
 
 
