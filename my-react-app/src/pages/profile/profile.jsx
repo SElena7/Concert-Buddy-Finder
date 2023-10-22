0da3237d66a620725f50c4470d81ca658feb6f78
SElena7 <89201050@student.upr.si>
2023-10-22 19:56:23 +0200

    Login page design created

---
+++ my-react-app/src/pages/profile/profile.jsx
@@ -0,0 +1,17 @@
+import './profile.scss'
+
+const Profile = () => {
+
+
+	return (
+
+		<div className="profile">
+			profile
+		</div>
+
+
+	)
+
+
+}
+export default Profile
---
+++ my-react-app/src/pages/register/register.jsx
@@ -0,0 +1,17 @@
+import './register.scss'
+
+const Register = () => {
+
+
+	return (
+
+		<div className="register">
+			register
+		</div>
+
+
+	)
+
+
+}
+export default Register
