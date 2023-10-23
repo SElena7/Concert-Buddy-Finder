38d705fa2ac212956ad112a2d925815e7ce8fe1a
SElena7 <89201050@student.upr.si>
2023-10-23 11:22:27 +0200

    Navbar design done.

--- my-react-app/src/components/navbar/navbar.jsx
+++ my-react-app/src/components/navbar/navbar.jsx
@@ -8,6 +8,8 @@ import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { Link } from "@mui/material";
+import PersonOutlineOutlined from "@mui/icons-material/PersonOutlineOutlined";
+import EmailOutlined from "@mui/icons-material/EmailOutlined";
 
 const Navbar = () => {
     return (
@@ -16,12 +18,27 @@ const Navbar = () => {
 
             <div className="left">
                 <Link to="/">
-                <span>Concert Buddy Finder</span>
-                </Link>    
+                    <span style={{ textDecoration:"none"}}>Concert Buddy Finder</span>
+                </Link>
+                <HomeOutlinedIcon />
+                <DarkModeOutlinedIcon />
+                <GridViewOutlinedIcon />
+
+                <div className="search">
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder="Search" />
+                </div>
+                
             </div>
 
             <div className="right">
-
+                <PersonOutlineOutlinedIcon />
+                <EmailOutlinedIcon />
+                <NotificationsNoneOutlinedIcon />
+                <div className="user">
+                     <img src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
+                    <span>John Doe</span>
+                </div>
             </div>
         </div>
 
