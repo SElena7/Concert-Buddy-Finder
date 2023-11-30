1ac01e4a642f2eba9d536a454bf32447d32dd1c9
SElena7 <89201050@student.upr.si>
2023-11-30 17:24:56 +0100

    Connected to database, API tests login, logout, register

--- api/index.js
+++ api/index.js
@@ -5,7 +5,13 @@ import postRoutes from "./routes/posts.js"
 import likeRoutes from "./routes/likes.js"
 import commentRoutes from "./routes/comments.js"
 import authRoutes from "./routes/auth.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+//middlewares
+app.use(Express.json())
+app.use(cors())
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
